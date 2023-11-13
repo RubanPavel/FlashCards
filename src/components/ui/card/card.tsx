65ce5e71d87fd7a9af1d1d080236bc3d3f5f13c0
admalla <maasovec@gmail.com>
2023-11-13 19:54:41 +0300

    add Card

---
+++ src/components/ui/card/card.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import s from './card.module.scss'
+
+export type CardProps = {
+  children: ReactNode
+  className?: string
+}
+
+export const Card = ({ children, className }: CardProps) => {
+  return (
+    <div className={`${s.card} ${className}`} >{children}</div>
+  )
+}
