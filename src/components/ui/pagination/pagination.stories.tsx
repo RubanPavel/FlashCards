20c81e9afc292d15282271a959a6bdc45976c598
Ruban Pavel <ruban.public@gmail.com>
2023-11-15 21:30:01 +0300

    format Pagination

--- src/components/ui/pagination/pagination.stories.tsx
+++ src/components/ui/pagination/pagination.stories.tsx
@@ -1,15 +1,16 @@
-import {Meta, StoryObj} from "@storybook/react";
-import {Pagination} from "@/components/ui/pagination/pagination";
-import {useState} from "react";
+import { useState } from 'react'
+
+import { Pagination } from '@/components/ui/pagination/pagination'
+import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
-  component: Pagination,
-  tags: ['autodocs'],
-  title: 'Components/Pagination',
   args: {
     currentPage: 1,
     totalCount: 100,
-  }
+  },
+  component: Pagination,
+  tags: ['autodocs'],
+  title: 'Components/Pagination',
 } satisfies Meta<typeof Pagination>
 
 export default meta
@@ -19,6 +20,13 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   render: args => {
     const [page, setPage] = useState(args.currentPage)
-    return <Pagination currentPage={page} totalCount={args.totalCount} onChange={(pageNumber) => setPage(pageNumber)} />
-  }
+
+    return (
+      <Pagination
+        currentPage={page}
+        onChange={pageNumber => setPage(pageNumber)}
+        totalCount={args.totalCount}
+      />
+    )
+  },
 }
--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -1,31 +1,32 @@
-import {ConfigProvider, Pagination as PaginationPage, Space, theme} from "antd";
-import {ComponentPropsWithoutRef} from "react";
+import { ComponentPropsWithoutRef } from 'react'
+
+import { ConfigProvider, Pagination as PaginationPage, Space, theme } from 'antd'
 
 export type Props = {
   className?: string
   currentPage: number
-  totalCount: number
   onChange: (pageNumber: number, pageSize: number) => void
+  totalCount: number
 } & ComponentPropsWithoutRef<typeof PaginationPage>
 
-export const Pagination = ({className, currentPage, totalCount, onChange}: Props) => {
+export const Pagination = ({ className, currentPage, onChange, totalCount }: Props) => {
   return (
     <ConfigProvider
       theme={{
+        algorithm: theme.darkAlgorithm,
         components: {
           Pagination: {
-            algorithm: true
-          }
+            algorithm: true,
+          },
         },
-        algorithm: theme.darkAlgorithm
       }}
     >
       <Space>
         <PaginationPage
           className={className}
           current={currentPage}
-          total={totalCount}
           onChange={onChange}
+          total={totalCount}
         />
       </Space>
     </ConfigProvider>
