afc5ad80de2b2052c03a89526e553936817e56a0
admalla <maasovec@gmail.com>
2023-11-18 06:15:46 +0300

    fix avatar

--- src/components/ui/avatar/avatar.stories.tsx
+++ src/components/ui/avatar/avatar.stories.tsx
@@ -10,10 +10,20 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
+export const withImage: Story = {
   args: {
     userName: 'User',
     imageUrl: "https://i.pinimg.com/736x/19/63/b2/1963b290b9856d479b432734029ff2ee.jpg"
+  },
+  render: (args) => {
+    return <AvatarRadix imageUrl={args.imageUrl} userName={args.userName} />
+  }
+}
+
+export const withoutImage: Story = {
+  args: {
+    userName: 'User',
+    imageUrl: ""
   },
   render: (args) => {
     return <AvatarRadix imageUrl={args.imageUrl} userName={args.userName} />
