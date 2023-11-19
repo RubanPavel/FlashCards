03a16a78b870c772ec12ac0621f9d8cb0e2479c9
annavilnid <anna.vilnid@gmail.com>
2023-11-19 12:12:21 +0100

    radio-group key, styles, stories

--- src/components/ui/radio-group/radio-group.stories.ts
+++ src/components/ui/radio-group/radio-group.stories.ts
@@ -3,7 +3,6 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { RadioGroup } from './'
 
 const meta = {
-  argTypes: {},
   component: RadioGroup,
   tags: ['autodocs'],
   title: 'Components/RadioGroup',
@@ -14,21 +13,8 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    defaultValue: 'Three',
     disabled: false,
-    labels: [
-      {
-        id: '1',
-        value: 'One',
-      },
-      {
-        id: '2',
-        value: 'Two',
-      },
-      {
-        id: '3',
-        value: 'Three',
-      },
-    ],
+    defaultValue: 'One',
+    options: ['One', 'Two', 'Three'],
   },
 }
--- src/components/ui/radio-group/radio-group.tsx
+++ src/components/ui/radio-group/radio-group.tsx
@@ -4,40 +4,41 @@ import * as RadioGroupRadixUi from '@radix-ui/react-radio-group'
 
 import s from './radio-group.module.scss'
 
-export type RadioGroupProps = {
+import { clsx } from 'clsx'
+
+import { Typography } from '@/components/ui/typography'
+
+export type Props = {
+  className?: string
   defaultValue?: string
   disabled?: boolean
-  labels: { id: string; value: string }[]
-  onChange?: () => void
+  options: string[]
 } & Omit<ComponentPropsWithoutRef<'div'>, 'dir'>
 
-export const RadioGroup = ({
-  className,
-  defaultValue,
-  disabled,
-  labels,
-  onChange,
-  ...rest
-}: RadioGroupProps) => {
+export const RadioGroup = ({ className, defaultValue, disabled, options, ...rest }: Props) => {
   return (
-    <form>
-      <RadioGroupRadixUi.Root
-        className={`${s.RadioGroupRoot} ${className}`}
-        defaultValue={defaultValue}
-        disabled={disabled}
-        {...rest}
-      >
-        {labels.map(l => (
-          <div className={s.RadioGroupWrapper}>
-            <RadioGroupRadixUi.Item className={s.RadioGroupItem} id={l.id} value={l.value}>
-              <RadioGroupRadixUi.Indicator className={s.RadioGroupIndicator} />
-            </RadioGroupRadixUi.Item>
-            <label aria-label={`${disabled && 'disabled'}`} className={s.Label} htmlFor={l.id}>
-              {l.value}
-            </label>
-          </div>
-        ))}
-      </RadioGroupRadixUi.Root>
-    </form>
+    <RadioGroupRadixUi.Root
+      className={`${s.root} ${className}`}
+      defaultValue={defaultValue}
+      disabled={disabled}
+      {...rest}
+    >
+      {options?.map(option => (
+        <div key={option} className={s.wrapper}>
+          <RadioGroupRadixUi.Item className={s.item} id={option} value={option}>
+            <RadioGroupRadixUi.Indicator className={s.indicator} />
+          </RadioGroupRadixUi.Item>
+          <Typography
+            className={s.label}
+            as={'label'}
+            htmlFor={option}
+            variant={'body-2'}
+            aria-label={clsx(disabled && 'disabled')}
+          >
+            {option}
+          </Typography>
+        </div>
+      ))}
+    </RadioGroupRadixUi.Root>
   )
 }
