9fe0324ac4d4768970ec56589f9014e25031b668
annavilnid <anna.vilnid@gmail.com>
2023-12-24 17:32:42 +0100

    add base-api-with-reauth

--- src/services/base-api-with-reauth.ts
+++ src/services/base-api-with-reauth.ts
@@ -1,5 +1,6 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query'
 import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query'
+
+import { fetchBaseQuery } from '@reduxjs/toolkit/query'
 import { Mutex } from 'async-mutex'
 
 const mutex = new Mutex()
@@ -9,23 +10,25 @@ const baseQuery = fetchBaseQuery({
 })
 
 export const baseQueryWithReauth: BaseQueryFn<
-  string | FetchArgs,
+  FetchArgs | string,
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
   await mutex.waitForUnlock()
   let result = await baseQuery(args, api, extraOptions)
+
   if (result.error && result.error.status === 401) {
     if (!mutex.isLocked()) {
       const release = await mutex.acquire()
       const refreshResult = await baseQuery(
-        { url: '/v1/auth/refresh-token', method: 'POST' },
+        { method: 'POST', url: '/v1/auth/refresh-token' },
         api,
         extraOptions
       )
+
       if (refreshResult.meta?.response?.status === 204) {
         result = await baseQuery(
-          { url: '/v1/auth/refresh-token', method: 'POST' },
+          { method: 'POST', url: '/v1/auth/refresh-token' },
           api,
           extraOptions
         )
@@ -36,5 +39,6 @@ export const baseQueryWithReauth: BaseQueryFn<
       result = await baseQuery(args, api, extraOptions)
     }
   }
+
   return result
 }
