f20f11a478250aa57812e5000c753b82b70a5d68
admalla <maasovec@gmail.com>
2023-12-26 01:01:10 +0300

    fix Slider

--- src/services/cards/cards.slice.ts
+++ src/services/cards/cards.slice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
 export type orderByQuestion = 'question-asc' | 'question-desc'
+
 type InitialState = {
   currentPage: number
   itemsPerPage: number
