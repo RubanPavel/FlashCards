d6003df2b32c4343071561269ef5af5f89c21040
annavilnid <anna.vilnid@gmail.com>
2023-12-26 19:35:38 +0100

    error type

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -2,19 +2,19 @@ import { toast } from 'react-toastify'
 
 import { FormValues, SignIn } from '@/components/auth/sign-in'
 import { useLoginMutation } from '@/services/auth'
+import { ServerError } from '@/services/error.types'
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
 
 import s from './login-page.module.scss'
-
 export const LoginPage = () => {
   const [login, {}] = useLoginMutation()
-
   const handleLogin = (formData: FormValues) => {
     login(formData)
       .unwrap()
-      // TODO типизация ошибки и как обрабатывать ошибки отсутствия сети, в с пустой? Случай успеха не обрабатываем?
-      //  Только ошибки? Или навигацию на главную
-      .catch((e: any) => {
-        toast.error(e?.data?.message || 'Error', { position: toast.POSITION.BOTTOM_CENTER })
+      .catch((e: ServerError & FetchBaseQueryError) => {
+        toast.error(e?.data?.message || 'Some error occurred', {
+          position: toast.POSITION.BOTTOM_CENTER,
+        })
       })
   }
 
