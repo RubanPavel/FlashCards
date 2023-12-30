ab255acf1d33fcfd7ac5b1b4affb82d7673ad82a
annavilnid <anna.vilnid@gmail.com>
2023-12-30 16:52:28 +0100

    some fix

--- src/pages/verify-email-page/verify-email-page.tsx
+++ src/pages/verify-email-page/verify-email-page.tsx
@@ -1,56 +1,46 @@
-import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { toast } from 'react-toastify'
+  import {useEffect, useState} from 'react'
+  import { useParams } from 'react-router-dom'
+  import { toast } from 'react-toastify'
 
-import { errorText } from '@/assets/variable'
-import { ConfirmEmail } from '@/components/auth/confirm-email'
-import { Loader } from '@/components/ui/loader'
-import { useVerifyEmailMutation } from '@/services/auth'
-import { ServerError } from '@/services/error.types'
+  import { errorText } from '@/assets/variable'
+  import { ConfirmEmail } from '@/components/auth/confirm-email'
+  import { Loader } from '@/components/ui/loader'
+  import { useVerifyEmailMutation } from '@/services/auth'
+  import { ServerError } from '@/services/error.types'
 
-import s from './verify-email.module.scss'
+  import s from './verify-email.module.scss'
 
-export const VerifyEmailPage = () => {
-  const { userId } = useParams<string>()
-  const [verifyMail, { data, isError, isLoading }] = useVerifyEmailMutation()
-  const trigger = !!data || !isError
+  export const VerifyEmailPage = () => {
+      const { userId } = useParams<string>();
+      const [verifyMail, { data, isError, isLoading }] = useVerifyEmailMutation();
+      const [requestSent, setRequestSent] = useState<boolean>(false);
+      const trigger = !!data || !isError;
 
-  // useEffect(() => {
-  //   const handleVerifyEmail = async () => {
-  //     if (userId) {
-  //       try {
-  //         await verifyMail({ code: userId }).unwrap()
-  //       } catch (e: unknown) {
-  //         const err = e as ServerError
-  //
-  //         toast.error(err?.data?.message || errorText, {
-  //           position: toast.POSITION.BOTTOM_CENTER,
-  //         })
-  //       }
-  //     }
-  //   }
-  //
-  //   handleVerifyEmail().then()
-  // }, [userId, verifyMail])
+      useEffect(() => {
+        const handleVerifyEmail = async () => {
+          try {
+            if (userId && !requestSent) {
+              await verifyMail({ code: userId });
+              setRequestSent(true);
+            }
+          } catch (e: unknown) {
+            const err = e as ServerError;
+            toast.error(err?.data?.message || errorText, {
+              position: toast.POSITION.BOTTOM_CENTER,
+            });
+          }
+        };
 
-  useEffect(() => {
-    const handleVerifyEmail = async () => {
-      try {
-        userId && (await verifyMail({ code: userId }));
-      } catch (e: unknown) {
-        const err = e as ServerError;
-        toast.error(err?.data?.message || errorText, {
-          position: toast.POSITION.BOTTOM_CENTER,
-        });
-      }
-    };
+        (async () => {
+          if (!isLoading) {
+            await handleVerifyEmail();
+          }
+        })();
+      }, [userId, verifyMail, requestSent, isLoading]);
 
-    handleVerifyEmail().then();
-  }, [userId, verifyMail]);
+    if (isLoading) {
+      return <Loader />
+    }
 
-  if (isLoading) {
-    return <Loader />
+    return <ConfirmEmail className={s.VerifyEmailPageRoot} trigger={trigger} />
   }
-
-  return <ConfirmEmail className={s.VerifyEmailPageRoot} trigger={trigger} />
-}
