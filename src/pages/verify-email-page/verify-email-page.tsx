c27865c399c8ebc6c5cdb7b74f59e1d8616d2bda
annavilnid <anna.vilnid@gmail.com>
2023-12-30 16:34:12 +0100

    some fix

--- src/pages/verify-email-page/verify-email-page.tsx
+++ src/pages/verify-email-page/verify-email-page.tsx
@@ -1,49 +1,53 @@
 import { useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
-import { errorText, verifyEmailPageData } from '@/assets/variable'
+import { errorText } from '@/assets/variable'
 import { ConfirmEmail } from '@/components/auth/confirm-email'
 import { Loader } from '@/components/ui/loader'
 import { useVerifyEmailMutation } from '@/services/auth'
 import { ServerError } from '@/services/error.types'
 
 import s from './verify-email.module.scss'
-import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
 
 export const VerifyEmailPage = () => {
-  const navigate = useNavigate()
   const { userId } = useParams<string>()
   const [verifyMail, { data, isError, isLoading }] = useVerifyEmailMutation()
-  const { info } = verifyEmailPageData
   const trigger = !!data || !isError
 
-  useEffect(() => {
-    console.log('test')
-    const handleVerifyEmail = async () => {
-      if (userId) {
-        try {
-          await verifyMail({ code: userId })
-            .unwrap()
-            .then(() => {
-              navigate('/login')
-              toast.success(info, {
-                position: toast.POSITION.BOTTOM_CENTER,
-              })
-            })
-            .catch((e: ServerError & FetchBaseQueryError) => {
-              toast.error(e?.data?.message || errorText, {
-                position: toast.POSITION.BOTTOM_CENTER,
-              })
-            })
-        } catch (e: unknown) {
-          return e
-        }
-      }
-    }
+  // useEffect(() => {
+  //   const handleVerifyEmail = async () => {
+  //     if (userId) {
+  //       try {
+  //         await verifyMail({ code: userId }).unwrap()
+  //       } catch (e: unknown) {
+  //         const err = e as ServerError
+  //
+  //         toast.error(err?.data?.message || errorText, {
+  //           position: toast.POSITION.BOTTOM_CENTER,
+  //         })
+  //       }
+  //     }
+  //   }
+  //
+  //   handleVerifyEmail().then()
+  // }, [userId, verifyMail])
 
-    handleVerifyEmail().then()
-  }, [])
+  useEffect(() => {
+    const handleVerifyEmail = async () => {
+      try {
+        userId && await verifyMail({ code: userId })
+      } catch (e: unknown) {
+        const err = e as ServerError
+
+                toast.error(err?.data?.message || errorText, {
+                  position: toast.POSITION.BOTTOM_CENTER,
+                })
+      }
+    };
+
+    handleVerifyEmail().then();
+  }, []);
 
   if (isLoading) {
     return <Loader />
